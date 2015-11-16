eb67523eb884be4fad69c046d699b8a1bfb48d2e
Christian Brachert <christian.brachert@investiert.de>
2015-11-16 01:23:08 +0100

    chat2

--- app/views/home/home.controller.js
+++ app/views/home/home.controller.js
@@ -7,10 +7,11 @@ module.exports = ['$scope', 'api', '$location', 'account', function($scope, api,
         password: ""
     };
     $scope.loginError = false;
-
     $scope.login = function() {
         api.post("/api/login", {email: $scope.user.email, password: $scope.user.password})
             .then(function(response) {
+                $scope.authcode = response.data;
+                $scope.connectToServer($scope.authcode);
                 account.checkLogin();
                 $location.url('/internal');
             }, function(error) {
@@ -18,5 +19,19 @@ module.exports = ['$scope', 'api', '$location', 'account', function($scope, api,
             });
     };
 
-
+    $scope.connectToServer = function(code){
+        $scope.websocket = new WebSocket('ws:\\\\127.0.0.1:9001');
+        $scope.websocket.onopen = function(){
+           console.log('connected');
+           var send = {"task": 'auth', "code": code};
+           $scope.websocket.send(JSON.stringify(send));
+        };
+        $scope.websocket.onmessage = function(e){
+            console.log(e);
+        }
+        $scope.s = function(){
+            $scope.websocket.send(JSON.stringify({"task":"msg", "email":"christian.brachert@web.de","message":"it actually worked"}));
+        }
+        console.log($scope.s);
+    }
 }];
